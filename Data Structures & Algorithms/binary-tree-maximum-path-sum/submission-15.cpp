fb71cfe7a1a5ca8c9abc5decdcee66750988110d
ArjunaCodes <mallikarjunaiah.k1@gmail.com>
2026-05-18 22:27:57 +0530

    Add: binary-tree-maximum-path-sum - submission-15

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-15.cpp
@@ -0,0 +1,87 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// class Solution {
+// public:
+//     int max_path_sum = INT_MIN;
+//     int maxPathSum(TreeNode* root) {
+//         find_max_sum(root);
+//         return max_path_sum;
+//     }
+//     int find_max_sum(TreeNode* root) {
+//         if(not root) return 0;
+//         int left = find_max_sum(root->left);
+//         int right = find_max_sum(root->right);
+//         max_path_sum = max((root->val + left + right), max_path_sum);
+//         // cout<< " max_path_sum " << max_path_sum <<" root->val " << root->val << endl;
+//         int sum = root->val + max(left, right);
+//         return max(sum, 0);
+//     }
+// };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// class Solution {
+// public:
+//     int maxPathSum(TreeNode* root) {
+//         int res = root->val;
+//         dfs(root, res);
+//         return res;
+//     }
+
+// private:
+//     int dfs(TreeNode* root, int& res) {
+//         if (!root) {
+//             return 0;
+//         }
+
+//         int leftMax = max(dfs(root->left, res), 0);
+//         int rightMax = max(dfs(root->right, res), 0);
+
+//         res = max(res, root->val + leftMax + rightMax);
+//         return root->val + max(leftMax, rightMax);
+//     }
+// };
+
+class Solution {
+public:
+    int dfs(TreeNode* root, int &res){
+        if(!root) return 0;
+
+        int leftMax = dfs(root->left,res);
+        int rightMax = dfs(root->right,res);
+
+        leftMax = max(leftMax,0);
+        rightMax = max(rightMax,0);
+
+        res = max(res, root->val + leftMax + rightMax);
+
+        return root->val+ max(leftMax,rightMax);
+    }
+
+    int maxPathSum(TreeNode* root) {
+        int res = root->val;
+        int ans = dfs(root,res);
+
+        return res;
+    }
+};
