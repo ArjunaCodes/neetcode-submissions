1c8757edae2a9676d4d3e2d37d12e0fb96c51d53
ArjunaCodes <mallikarjunaiah.k1@gmail.com>
2026-05-19 15:43:54 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,22 @@
+class KthLargest {
+    vector<int> elements;
+    int k; 
+public:
+    KthLargest(int k, vector<int>& nums):k(k) {
+        for(const auto& num: nums) {
+            elements.push_back(num);
+        }
+        sort(elements.begin(), elements.end(), greater<int>());
+        while(elements.size() > k) {
+            elements.pop_back();
+        }
+    }
+    
+    int add(int val) {
+        elements.emplace_back(val);
+        sort(elements.begin(), elements.end(), greater<int>());
+        if(elements.size() > k)
+            elements.pop_back();
+        return elements[k-1];
+    }
+};
