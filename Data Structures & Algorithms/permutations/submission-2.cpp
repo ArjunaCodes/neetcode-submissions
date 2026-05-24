2a38be3712ea9879e9b42651efadd203fa323ccd
ArjunaCodes <mallikarjunaiah.k1@gmail.com>
2026-05-24 15:04:05 +0530

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    vector<vector<int>> res;
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        backtrack(nums, 0);
+        return res;
+    }
+
+    void backtrack(vector<int>& nums, int idx) {
+        if (idx == nums.size()) {
+            // cout<< " considered " <<endl;
+            // print_state(nums);
+            res.push_back(nums);
+            return;
+        }
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[idx], nums[i]);
+            // cout<<" i " << i << " idx " << idx << " ";
+            // print_state(nums);
+            backtrack(nums, idx + 1);
+            // cout<<endl;
+            swap(nums[idx], nums[i]);
+        }
+    }
+};
