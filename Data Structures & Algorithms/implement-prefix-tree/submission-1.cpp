ba2d89690d9d8ec7a16a28daa5503e5ac1cd0548
ArjunaCodes <mallikarjunaiah.k1@gmail.com>
2026-05-28 12:54:02 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.cpp
@@ -0,0 +1,52 @@
+class PrefixTree {
+public:
+    struct trie {
+        trie* childs[26];
+        trie(): is_end_of_word{false} {
+            for(int i=0; i < 26; ++i) {
+                childs[i] = nullptr;
+            }
+        }
+        bool is_end_of_word;
+    };
+    trie* root;
+    PrefixTree() {
+        root = new trie();
+    }
+    
+    void insert(string word) {
+        trie* curr = root;
+        for(auto ch: word) {
+            int index = ch - 'a';
+            if(not curr->childs[index]){
+                curr->childs[index] = new trie();
+            }
+            curr = curr->childs[index];
+        }
+        curr->is_end_of_word = true;
+    }
+    
+    bool search(string word) {
+        trie* curr = root;
+        for(auto ch: word) {
+            int index = ch - 'a';
+            if(not curr->childs[index]) {
+                return false;
+            }
+            curr = curr->childs[index];
+        }
+        return curr->is_end_of_word;
+    }
+    
+    bool startsWith(string prefix) {
+        trie* curr = root;
+        for(auto ch: prefix) {
+            int index = ch - 'a';
+            if(not curr->childs[index]) {
+                return false;
+            }
+            curr = curr->childs[index];
+        }
+        return true;
+    }
+};
