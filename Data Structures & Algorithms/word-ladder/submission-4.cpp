040922482364d07130c836937d740a219cb146d7
ArjunaCodes <mallikarjunaiah.k1@gmail.com>
2026-06-13 17:45:43 +0530

    Add: word-ladder - submission-4

---
+++ Data Structures & Algorithms/word-ladder/submission-4.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int min_count = INT_MAX;
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        bool end_present = false;
+        for(const auto& word: wordList) {
+            if(word == endWord) {
+                end_present = true;
+                break;
+            }
+        }
+        if(not end_present) return 0;
+        int word_len = beginWord.size();
+        queue<string> que;
+        que.push(beginWord);
+        unordered_set<string> pro;
+        int count = 0;
+        int reachable = false;
+        while(not que.empty()) {
+            int size = que.size();
+            count++;
+            bool brk = false;
+            while(size--) {
+                string word = que.front();
+                if(word == endWord){
+                    reachable = true;
+                    brk = true;
+                    break;
+                }
+                que.pop();
+                for(int i=0; i<word_len; ++i) {
+                    string temp_copy = word;
+                    temp_copy[i] = '*';
+                    for(const auto& wd: wordList) {
+                        string tmp = wd;
+                        tmp[i] = '*';
+                        if(temp_copy == tmp) {
+                            // adj_list[word].push_back(wd);
+                            if(pro.count(wd) == 0){
+                                pro.insert(wd);
+                                que.push(wd);
+                            }
+                        }
+                    }
+                }
+            }
+            if(brk) break;
+        }
+        return reachable ? count : 0;
+    }
+};
